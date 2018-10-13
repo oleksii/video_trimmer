7b7d5ecb31a081c7a8c9cf097362e63534409e71
Oleksii Korondevych <oleksii.korondevych@gmail.com>
2018-10-13 22:59:44 +0300

    Added RSpec to the project

--- video_trimmer/config/routes.rb
+++ video_trimmer/config/routes.rb
@@ -1,6 +1,8 @@
 require 'api_constraints'
 
 VideoTrimmer::Application.routes.draw do
+  mount Raddocs::App => "/docs"
+
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     end
---
+++ video_trimmer/spec/acceptance_helper.rb
@@ -0,0 +1,11 @@
+require 'rspec_api_documentation'
+require 'rspec_api_documentation/dsl'
+
+RspecApiDocumentation.configure do |config|
+  config.format                      = :json
+  config.curl_host                   = 'http://localhost'
+  config.request_body_formatter      = :json
+  config.curl_headers_to_filter      = ['Host', 'Cookie']
+  config.request_headers_to_include  = ['Content-Type', 'Accept']
+  config.response_headers_to_include = ['Content-Type']
+end
